8c413371facc3a055f7066bb61eab2ff6733c7c8
Andrew <andrewrr@Andrejs-MacBook-Air.local>
2020-01-29 19:06:28 +0200

    upd

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseUrl: 'https://api.unsplash.com',
+  baseURL: 'https://api.unsplash.com',
   headers: {
     Authorization: 'Client-ID fe23936d559a1890c5a706836d1c3fedb39993ca89b0a9c87fcde61f47f784a3'
   }
