9be91197a3a325ff738a955c5081e2bc1740f637
Andrew <andrewrr@Andrejs-MacBook-Air.local>
2020-01-29 18:05:32 +0200

    upd

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import axios from 'axios';
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+  state = { images: [] }
+
+   onSearchSubmit = async (term) => {
+    const resp = await axios.get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+          Authorization: 'Client-ID fe23936d559a1890c5a706836d1c3fedb39993ca89b0a9c87fcde61f47f784a3'
+        }
+    });
+
+    this.setState({ images: resp.data.results });
+  }
+
+  render() {
+    return <div className="cmp-app ui container">
+      <SearchBar onSubmit={this.onSearchSubmit} />
+
+      Found {this.state.images.length} images.
+    </div>
+  }
+}
+
+export default App;
