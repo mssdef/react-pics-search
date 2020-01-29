20df5f5efd55456b5295ce83c9774376eb912540
Andrew <andrewrr@Andrejs-MacBook-Air.local>
2020-01-29 18:08:18 +0200

    upd

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
           Authorization: 'Client-ID fe23936d559a1890c5a706836d1c3fedb39993ca89b0a9c87fcde61f47f784a3'
         }
     });
-
+ 
     this.setState({ images: resp.data.results });
   }
 
